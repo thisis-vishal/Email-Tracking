19b7249c2fa3d03a77a684ad62f2c087c3aabddb
Vishal Kumar <kushwaha.evil600@gmail.com>
2023-05-26 13:51:45 +0530

    initialized django project

--- emailtracker/emailtracker/settings.py
+++ emailtracker/emailtracker/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'etarc',
+    'etrack',
     'rest_framework',
     'corsheaders',
 ]
