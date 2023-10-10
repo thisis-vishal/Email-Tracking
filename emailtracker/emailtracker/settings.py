28a657249398110fb11b7131fbb3317b762b9c67
Vishal Kumar <76644828+thisis-vishal@users.noreply.github.com>
2023-10-11 00:56:10 +0530

    Update settings.py
    set debugging to False

--- emailtracker/emailtracker/settings.py
+++ emailtracker/emailtracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cvmko^vg@_6@(&8k7vl*dkq^_fep3cw$e#(4rd93%+m1eg@wq@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['email-tracking-five.vercel.app','127.0.0.1']
 
