6b2946fda1b7435641216fe2a9d97734bd24c9d1
Vishal Kumar <kushwaha.evil600@gmail.com>
2023-11-27 14:40:10 +0530

    added date field in model

--- emailtracker/etrack/models.py
+++ emailtracker/etrack/models.py
@@ -6,5 +6,6 @@ class emailData(models.Model):
     email = models.EmailField(max_length = 254 , blank= True , null=True)
     status = models.IntegerField(default=0)
     unique_code = models.UUIDField(null=True, blank=True , unique=True)
+    open_date=models.DateTimeField(null=True)
     def __str__(self):
         return self.email
